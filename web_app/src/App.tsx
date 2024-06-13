640d9fb72378f103af06891e46cd621886129e96
floorfier <jose.vera@floorfy.com>
2024-06-13 16:49:53 +0200

    Remove header and set i18n debug to false

--- web_app/src/App.tsx
+++ web_app/src/App.tsx
@@ -63,7 +63,6 @@ function Home() {
   return (
     // <main className="flex flex-col items-center justify-between w-full min-h-screen bg-black">
     <main className="flex min-h-screen flex-col items-center justify-between w-full bg-[radial-gradient(circle_at_1px_1px,_#8e8e8e8e_1px,_transparent_0)] [background-size:20px_20px] bg-repeat">
-      <Header />
       <Workspace />
     </main>
   )
