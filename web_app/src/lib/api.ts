f1c6676e0c93c8fe64992cba92d0c233951cc377
floorfier <jose.vera@floorfy.com>
2024-06-18 10:10:21 +0200

    Update API endpoint

--- web_app/src/lib/api.ts
+++ web_app/src/lib/api.ts
@@ -11,7 +11,7 @@ import { convertToBase64, srcToFile } from "@/lib/utils"
 import axios from "axios"
 
 // export const API_ENDPOINT = "https://albacore-touched-pony.ngrok-free.app/api/v1"
-export const API_ENDPOINT = "https://0620a8cfde62ef.lhr.life/api/v1"
+export const API_ENDPOINT = "https://5fe8d675881794.lhr.life/api/v1"
 // export const API_ENDPOINT = "http://127.0.0.1:8080/api/v1"
 
 const api = axios.create({
